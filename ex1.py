3489528a7f8dc2ca907d08be76756e1a05add9af
lbunge <lukebunge@live.com>
2018-02-20 19:43:19 -0500

    Learn Python the Hard Way

---
+++ ex1.py
@@ -0,0 +1,8 @@
+#Python script to learn the hard way
+print("Hello World!\n \n")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this')
