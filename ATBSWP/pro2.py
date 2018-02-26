db44caf3a4adb615c4f8f96f33a6b4ccf78a0f07
lbunge <lukebunge@live.com>
2018-02-25 19:04:43 -0500

    Learning Automate the Boring Stuff with Python

---
+++ ATBSWP/pro2.py
@@ -0,0 +1,12 @@
+# This program practices flow control
+
+print('What is your name')
+name = input()
+if name == 'Mary':
+    print('Hello Mary')
+print('Please enter password')
+password = input()
+if password == 'swordfish':
+    print('Access granted')
+else:
+    print('Wrong password.')
